68ce0920b7c40e6694e0c31d102b20ed85034170
mayoljojo <mayoljojo@gmail.com>
2024-08-14 00:04:38 +0800

    Prepare data-table for products page

---
+++ app/(primary_pages)/products/data-table/columns.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDown } from "lucide-react";
+import { ProductFormSchema } from "@/zod-schema/schema";
+import { z } from "zod";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Product = {
+  id: number; // Changed from string to number
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  categoryId: number;
+  tags: string | null; // Changed from string to string | null
+};
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "price",
+    header: () => <div>Price</div>,
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-PH", {
+        style: "currency",
+        currency: "PHP",
+      }).format(price);
+
+      return <div>{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+  },
+  {
+    id: "actions",
+    header: () => <div>Actions</div>,
+    cell: ({ row }) => {
+      const product = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(product.id.toString())
+              }
+            >
+              Copy product ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
